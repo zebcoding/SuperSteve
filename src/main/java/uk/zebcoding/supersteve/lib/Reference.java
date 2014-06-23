9f92a17baeffb393d6c2f2704e62cbf23a0a4f83
Charlotte <charlotte@zebington.co.uk>
2014-06-23 21:49:03 +0100

    Crafting and functionality updates, ready 0.1.0a release,

--- src/main/java/uk/zebcoding/supersteve/lib/Reference.java
+++ src/main/java/uk/zebcoding/supersteve/lib/Reference.java
@@ -6,6 +6,6 @@ package uk.zebcoding.supersteve.lib;
 public class Reference {
   public static final String SS_ID = "supersteve";
   public static final String SS_NAME = "Super Steve";
-  public static final String SS_VERSION = "1.7.2-10.12.2.1126-0.1.0";
+  public static final String SS_VERSION = "1.7.2-10.12.2.1126-0.1.0a";
   public static final String SS_MCVERSION = "1.7.2";
 }
